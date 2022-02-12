6e1e3c5df12ac05f3410c9839562c470f8b926e9
kncr-f <70473281+kncr-f@users.noreply.github.com>
2022-02-12 21:52:43 +0100

    adding bottom ticker and some optimisations

---
+++ ticker_ajax/ticker_ajax/script.js
@@ -0,0 +1,148 @@
+(function () {
+
+   $.ajax({
+      url: '/data.json',
+      method: 'GET',
+      data: {
+         limit: 20
+      },
+      success: function (data) {
+         var ticker = "";
+         var ticker2 = "";
+         for (var i = 0; i < data.length; i++) {
+            console.log("data[i]", data[i]);
+            var href = data[i].link;
+            var city = "<a href='" + href + "' class='item1'>" + data[i].myText + "</a>";
+            var city2 = "<a href='" + href + "' class='items'>" + data[i].myText + "</a>";
+            ticker += city;
+            ticker2 += city2;
+         }
+         console.log("myHtml", ticker);
+         $("#headlines").html(ticker);
+
+
+         var jQheadlines = $('#headlines');
+
+         var jQleft = jQheadlines.offset().left;
+
+
+         var linksTop = $(".item1");
+
+         var reqId;
+
+         function moveHeadlines() {
+            jQleft--;
+
+
+            if (jQleft <= - linksTop.eq(0).outerWidth()) {
+               // console.log(linksTop.eq(0).outerWidth())
+               jQleft += linksTop.eq(0).outerWidth();
+
+               jQheadlines.append(linksTop.eq(0));
+               linksTop = $("a");
+
+
+            }
+
+            jQheadlines.css({
+               left: jQleft + "px"
+            })
+
+            reqId = requestAnimationFrame(moveHeadlines);
+
+
+         }
+
+         moveHeadlines();
+
+
+
+         linksTop.on("mouseenter", function fn(e) {
+            cancelAnimationFrame(reqId);
+            $(e.currentTarget).css({
+               color: "blue"
+            })
+
+         })
+         linksTop.on("mouseleave", function fn(e) {
+            moveHeadlines();
+            $(e.currentTarget).css({
+               color: "tomato"
+            })
+
+         })
+
+
+         //ticker_bottom
+
+         $("#headlines2").html(ticker2);
+
+         var jQmyTicker = $("#headlines2");
+         console.log(jQmyTicker[0]);
+
+         var jQleftBottom = jQmyTicker.offset().left;
+
+
+         var linksBottom = $(".items");
+         console.log(linksBottom.length);
+         console.log(linksBottom.eq(6));
+
+         var delId;
+
+         console.log(jQleftBottom)
+
+         function moveTicker() {
+            jQleftBottom++;
+
+
+
+            if (jQleftBottom >= 0) {
+
+               jQleftBottom -= linksBottom.eq(linksBottom.length - 1).outerWidth();
+               jQmyTicker.prepend(linksBottom.eq(linksBottom.length - 1));
+               linksBottom = $(".items")
+
+
+            }
+
+            jQmyTicker.css({
+               left: jQleftBottom + "px"
+            })
+
+
+            delId = requestAnimationFrame(moveTicker)
+         }
+
+         moveTicker();
+
+
+
+         linksBottom.on("mouseenter", function fn(e) {
+            cancelAnimationFrame(delId);
+            $(e.currentTarget).css({
+               color: "darkblue"
+            })
+
+         })
+
+         linksBottom.on("mouseleave", function fn(e) {
+            moveTicker();
+            $(e.currentTarget).css({
+               color: "brown"
+            })
+
+         })
+
+
+
+
+
+
+      }
+   });
+
+
+
+
+
+})()
